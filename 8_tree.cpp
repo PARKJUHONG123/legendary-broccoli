1033e804ddea3befa1437e95fc3c3ceab1620839
PARKJUHONG123 <corallines@naver.com>
2020-03-14 18:28:09 +0900

    memory remain problem in tree.cpp resolve

--- 8_tree.cpp
+++ 8_tree.cpp
@@ -84,7 +84,6 @@ namespace queue_space {
 				head = head->next;
 				delete temp;
 			}
-			cout << "큐가 삭제됨" << endl;
 		}
 	};
 }
@@ -97,24 +96,18 @@ struct node {
 	node<T>* right = nullptr;
 };
 
+// 삭제 과정에서 메모리 잔여 문제가 발생해서 다시 코드 작성
 template <typename T>
 class binary_tree {
 private:
-	node<T>* root;
+	node<T> *root;
+
 public:
-	binary_tree() : root(nullptr) {
+	binary_tree() : root(nullptr){
 
 	}
 	~binary_tree() {
-		if (root != nullptr) deleteTree(root);
-		cout << "트리 삭제" << endl;
-	}
-
-	void deleteTree(node<T>* root) {
-		if (root->left != nullptr) deleteTree(root->left);
-		if (root->right != nullptr) deleteTree(root->right);
-		delete []root;
-		return;
+		deleteTree();
 	}
 
 	node<T>* getRoot() {
@@ -122,480 +115,253 @@ public:
 	}
 
 	void add(T value) {
-		node<T>* index = root;
+		node<T>* search_node = root;
 		node<T>* temp = new node<T>;
+		temp->left = nullptr;
+		temp->right = nullptr;
 		temp->value = value;
 
-		while (1) {
-			if (index == nullptr) {
-				root = temp;
-				break;
-			}
-			else {
-				if (index->value > temp->value) {
-					if (index->left == nullptr) {
-						index->left = temp;
+		if (root == nullptr) {
+			root = temp;
+		}
+		else {
+			while (1) {
+				if (search_node->value < temp->value) {
+					if (search_node->right == nullptr) {
+						search_node->right = temp;
 						break;
 					}
 					else {
-						index = index->left;
+						search_node = search_node->right;
 					}
 				}
-				else if (index->value < temp->value) {
-					if (index->right == nullptr) {
-						index->right = temp;
+				else if (search_node->value > temp->value){
+					if (search_node->left == nullptr) {
+						search_node->left = temp;
 						break;
 					}
 					else {
-						index = index->right;
+						search_node = search_node->left;
 					}
 				}
 				else {
-					cout << "중복된 값 발견 : " << index->value << " = " << temp->value << endl;
-					delete[]temp;
+					cout << "add error";
 					break;
 				}
 			}
 		}
 	}
 
-	node<T>* search(node<T>* root) {
-		if (root == nullptr) {
-			return nullptr;
+	T remove(T value) {
+		bool leftFromParent = false;
+		node<T>* parent = nullptr;
+		node<T>* target = root;
+		if (target == nullptr) {
+			cout << "remove error : root nullptr";
+			return NULL;
 		}
-		if (root->left == nullptr) { //자기 자신이 왼쪽 끝이라면
-			return root;
-		}
-		else {
-			return search(root->left); //더 왼쪽 끝이 있다면
-		}
-	}
 
-	node<T>* remove(node<T>* root, node<T>* parent, T target) {
-		node<T>* curr = nullptr;
-		if (root == nullptr) {
-			return nullptr;
+		while (1) {
+			if (target->value < value) {
+				if (target->right == nullptr) {
+					cout << "remove error : no child";
+					return NULL;
+				}
+				parent = target;
+				leftFromParent = false;
+				target = target->right;
+			}
+			else if (target->value > value) {
+				if (target->left == nullptr) {
+					cout << "remove error : no child";
+					return NULL;
+				}
+				parent = target;
+				leftFromParent = true;
+				target = target->left;
+			}
+			else {
+				break;
+			}
 		}
+
+		// 이제 target 이 바로 value 값을 가진 node
 		
-		if (root->value > target) { // 타겟보다 값이 크거나
-			curr = remove(root->left, root, target);
-		}
-		else if (root->value < target) { // 타켓보다 값이 작거나
-			curr = remove(root->right, root, target);
-		}
-		else { // 타겟일 경우
-			curr = root;
-
-			// 자식 노드 없음
-			if (root->left == nullptr && root->right == nullptr) {
-				// 똑 때버림
-				if (parent->left == root) {
-					parent->left = nullptr;
-				}
-				else {
-					parent->right = nullptr;
-				}
-				delete[]root;
-				cout << "no child node delete" << endl;
+		// 고려해야 할 것 target = root 일 때 
+		if (parent == nullptr) { // 즉 target == root
+			if (target->left == nullptr && target->right == nullptr) { //무자식
+				delete[]target;
+				root = nullptr;
+				cout << "empty tree" << endl;
 			}
-
-			// 자식 노드 두개
-			else if (root->left != nullptr && root->right != nullptr) {
-				// 오른쪽 중에서 가장 왼쪽에 있는 값을 선택한다
-				node<T>* min_node = search(root->right);
-				min_node = remove(root, nullptr, min_node->value);
-				root->value = min_node->value;
-				delete[]min_node;
-				cout << "two child node delete" << endl;
-			}
-
-			// 자식 노드 한개
-			else {
-				node<T>* temp = (root->left != nullptr) ? root->left : root->right;
-				if (parent == nullptr) {
-					root->left = temp->left;
-					root->right = temp->right;
-					root->value = temp->value;
-					delete[]temp;
-				}
-				else {
-					if (parent->left == root) {
-						parent->left = temp;
+			else if (target->left != nullptr && target->right != nullptr){ //자식2
+				node<T>* temp_child, * temp_parent;
+				temp_child = target->right;
+				temp_parent = target;			
+				while (1) {
+					if (temp_child->left != nullptr) {
+						temp_parent = temp_child;
+						temp_child = temp_child->left;
 					}
 					else {
-						parent->right = temp;
-					}
-					delete[]root;
-				}
-				cout << "one child node delete" << endl;
-			}
-		}
-		return curr;
-	}
-
-
-	void preorder(node<T>* index) {
-		cout << index->value << " ";
-		if (index->left != nullptr) {
-			preorder(index->left);
-		}
-		if (index->right != nullptr) {
-			preorder(index->right);
-		}
-	}
-
-	void inorder(node<T>* index) {
-		if (index->left != nullptr) {
-			inorder(index->left);
-		}
-		cout << index->value << " ";
-		if (index->right != nullptr) {
-			inorder(index->right);
-		}
-	}
-
-	void postorder(node<T>* index) {
-		if (index->left != nullptr) {
-			postorder(index->left);
-		}
-		if (index->right != nullptr) {
-			postorder(index->right);
-		}
-		cout << index->value << " ";
-	}
-
-	void levelorder(node<T>* index) {
-		queue_space::queue<node<T>> queue = queue_space::queue<node<T>>();
-		queue.enqueue(*index);
-		node<T> node;
-
-		while (queue.returnHead() != nullptr) {
-			node = queue.dequeue();
-			cout << node.value << " ";
-			if (node.left != nullptr) {
-				queue.enqueue(*(node.left));
-			}
-			if (node.right != nullptr) {
-				queue.enqueue(*(node.right));
-			}
-		}
-	}
-};
-
-
-/*
-1. Root Property : Root 노드의 색깔은 검정이다
-2. External Property : Leaf 노드의 색깔은 검정이다
-3. Internal Property : Red 노드의 자식은 검정이다
-4. Depth Property : 모든 Leaf 노드에서 Black depth 는 같다
-*/
-
-template <typename T>
-class red_black_tree {
-private:
-	node<T>* root;
-public:
-	red_black_tree() {
-		root = nullptr;
-	}
-
-	node<T>* getRoot() {
-		return root;
-	}
-
-	void add(T value) {
-		node<T>* v = root;
-		node<T>* wv_parent = nullptr;
-		node<T>* w = nullptr;
-		node<T>* z = new node<T>;
-		z->value = value;
-		z->color = RED;
-
-		while (1) {
-			if (v == nullptr) {
-				z->color = BLACK;
-				root = z;
-				break;
-			}
-			else {
-				if (v->value > z->value) {
-					if (v->left == nullptr) { // 집어넣기
-
-						if (wv_parent != nullptr) {
-							if (wv_parent->left == v) {
-								w = wv_parent->right;
-								if (w == nullptr || w->color == BLACK) {
-									restructing(z, v, wv_parent);
-								}
-								else {
-									recoloring(w, z, v, wv_parent);
-								}
-							}
-							else {
-								w = wv_parent->left;
-								if (w == nullptr || w->color == BLACK) {
-									restructing(z, v, wv_parent);
-								}
-								else {
-									recoloring(w, z, v, wv_parent);
-								}
-							}
-						}
-						v->left = z;
 						break;
 					}
-					else {
-						wv_parent = v;
-						v = v->left;
-					}
 				}
-				else if (v->value < z->value) {
-					if (v->right == nullptr) { // 집어넣기
 
-						if (wv_parent != nullptr) {
-							if (wv_parent->left == v) {
-								w = wv_parent->right;
-								if (w == nullptr || w->color == BLACK) {
-									restructing(z, v, wv_parent);
-								}
-								else {
-									recoloring(w, z, v, wv_parent);
-								}
-							}
-							else {
-								w = wv_parent->left;
-								if (w == nullptr || w->color == BLACK) {
-									restructing(z, v, wv_parent);
-								}
-								else {
-									recoloring(w, z, v, wv_parent);
-								}
-							}
-						}
-
-						v->right = z;
-						break;
-					}
-					else {
-						wv_parent = v;
-						v = v->right;
-					}
+				if (temp_parent == target) {
+					target->value = temp_child->value;
+					temp_parent->right = temp_child->right;
+					delete[] temp_child;
 				}
 				else {
-					cout << "중복된 값 발견 : " << v->value << " = " << z->value << endl;
-					delete[]z;
+					if (temp_child->right == nullptr) {
+						target->value = temp_child->value;
+						temp_parent->left = nullptr;
+						delete[]temp_child;
+					}
+					else {
+						target->value = temp_child->value;
+						temp_parent->left = temp_child->right;
+						delete[]temp_child;
+					}
+				}
+			}
+			else { // 자식1
+				if (target->left == nullptr) {
+					root = target->right;
+					delete[] target;
+				}
+				else {
+					root = target->left;
+					delete[] target;
+				}
+			}
+			return value;
+		}
+
+
+
+		// 고려해야 할 것 : 무자식, 자식2, 자식1
+		if (target->left == nullptr && target->right == nullptr) { // 무자식
+			if (leftFromParent == true) {
+				parent->left = nullptr;
+				delete[] target;
+			}
+			else {
+				parent->right = nullptr;
+				delete[] target;
+			}
+		}
+		else if (target->left != nullptr && target->right != nullptr) { // 자식2
+			node<T>* temp_child, *temp_parent;
+			temp_child = target->right;
+			temp_parent = target;
+			while (1) {
+				if (temp_child->left != nullptr) {
+					temp_parent = temp_child;
+					temp_child = temp_child->left;
+				}
+				else {
 					break;
 				}
 			}
-		}
-	}
 
-	void restructing(node<T>* z, node<T>* v, node<T>* wv_parent) {
-		node<T> *left = nullptr, *right = nullptr, *parent = nullptr;
-		node<T>* wv = new node<T>;
-
-		wv->color = wv_parent->color;
-		wv->left = wv_parent->left;
-		wv->right = wv_parent->right;
-		wv->value = wv_parent->value;
-
-		if (z->value < v->value && v->value < wv->value) {
-			left = z;
-			parent = v;
-			right = wv;
-		}
-		else if (z->value < wv->value && wv->value < v->value) {
-			left = z;
-			parent = wv;
-			right = v;
-		}
-		else if (v->value < z->value && z->value < wv->value) {
-			left = v;
-			parent = z;
-			right = wv;
-		}
-		else if (v->value < wv->value && wv->value < z->value) {
-			left = v;
-			parent = wv;
-			right = z;
-		}
-		else if (wv->value < z->value && z->value < v->value) {
-			left = wv;
-			parent = z;
-			right = v;
-		}
-		else  { // if (wv->value < v->value && v->value < z->value)
-			left = wv;
-			parent = v;
-			right = z;
-		}
-
-
-		if (wv->left == v) {
-			left->left = nullptr;
-			right->right = wv->right;
-		}
-		else {
-			left->left = wv->left;
-			right->right = nullptr;
-		}
-		left->right = nullptr;
-		right->left = nullptr;
-
-		left->color = RED;
-		right->color = RED;
-		parent->color = BLACK;
-
-		parent->left = left;
-		parent->right = right;
-
-		wv_parent->color = parent->color;
-		wv_parent->left = parent->left;
-		wv_parent->right = parent->right;
-		wv_parent->value = parent->value;
-		delete[]parent;
-	}
-
-	void recoloring(node<T>* w, node<T>* z, node<T>* v, node<T>* wv_parent) {
-		w->color = BLACK;
-		v->color = BLACK;
-		wv_parent->color = RED;
-
-		if (wv_parent != root) {
-			if (w->color == RED) {
-				recoloring(w, z, v, wv_parent);
+			if (temp_parent == target) {
+				target->value = temp_child->value;
+				temp_parent->right = temp_child->right;
+				delete[] temp_child;
 			}
 			else {
-				restructing(z, v, wv_parent);
-			}
-		}
-		else {
-			wv_parent->color = BLACK;
-		}
-	}
-
-	node<T>* remove(node<T>* root, node<T>* parent, T target) {
-		node<T>* curr = nullptr;
-		if (root == nullptr) {
-			return nullptr;
-		}
-
-		if (root->value > target) { // 타겟보다 값이 크거나
-			curr = remove(root->left, root, target);
-		}
-		else if (root->value < target) { // 타켓보다 값이 작거나
-			curr = remove(root->right, root, target);
-		}
-		else { // 타겟일 경우
-			curr = root;
-
-			// 자식 노드 없음
-			if (root->left == nullptr && root->right == nullptr) {
-				// 똑 때버림
-				if (parent->left == root) {
-					parent->left = nullptr;
+				if (temp_child->right == nullptr) {
+					target->value = temp_child->value;
+					temp_parent->left = nullptr;
+					delete[]temp_child;
 				}
 				else {
-					parent->right = nullptr;
+					target->value = temp_child->value;
+					temp_parent->left = temp_child->right;
+					delete[]temp_child;
 				}
-				delete[]root;
-				cout << "no child node delete" << endl;
 			}
-
-			// 자식 노드 두개
-			else if (root->left != nullptr && root->right != nullptr) {
-				// 오른쪽 중에서 가장 왼쪽에 있는 값을 선택한다
-				node<T>* min_node = search(root->right);
-				min_node = remove(root, nullptr, min_node->value);
-				root->value = min_node->value;
-				delete[]min_node;
-				cout << "two child node delete" << endl;
+		}
+		else { // 자식1
+			if (leftFromParent == true) {
+				if (target->left == nullptr) {
+					parent->left = target->right;
+					delete[] target;
+				}
+				else {
+					parent->left = target->left;
+					delete[] target;
+				}
 			}
-
-			// 자식 노드 한개
 			else {
-				node<T>* temp = (root->left != nullptr) ? root->left : root->right;
-				if (parent == nullptr) {
-					root->left = temp->left;
-					root->right = temp->right;
-					root->value = temp->value;
-					delete[]temp;
+				if (target->left == nullptr) {
+					parent->right = target->right;
+					delete[] target;
 				}
 				else {
-					if (parent->left == root) {
-						parent->left = temp;
-					}
-					else {
-						parent->right = temp;
-					}
-					delete[]root;
+					parent->right = target->left;
+					delete[] target;
 				}
-				cout << "one child node delete" << endl;
 			}
 		}
-		return curr;
+		return value;
 	}
 
-
-
-
-
-
-
-
-
-
-
-	void preorder(node<T>* index) {
-		cout << index->value << " ";
-		if (index->left != nullptr) {
-			preorder(index->left);
-		}
-		if (index->right != nullptr) {
-			preorder(index->right);
-		}
-	}
-
-	void inorder(node<T>* index) {
-		if (index->left != nullptr) {
-			inorder(index->left);
-		}
-		cout << index->value << " ";
-		if (index->right != nullptr) {
-			inorder(index->right);
-		}
-	}
-
-	void postorder(node<T>* index) {
-		if (index->left != nullptr) {
-			postorder(index->left);
-		}
-		if (index->right != nullptr) {
-			postorder(index->right);
-		}
-		cout << index->value << " ";
-	}
-
-	void levelorder(node<T>* index) {
-		queue_space::queue<node<T>> queue = queue_space::queue<node<T>>();
-		queue.enqueue(*index);
-		node<T> node;
-
-		while (queue.returnHead() != nullptr) {
-			node = queue.dequeue();
-			cout << node.value << " ";
-			if (node.left != nullptr) {
-				queue.enqueue(*(node.left));
+	void deleteTree() {
+		node<T>* temp;
+		while (1) {
+			temp = getRoot();
+			if (temp == nullptr) {
+				break;
 			}
-			if (node.right != nullptr) {
-				queue.enqueue(*(node.right));
+			else {
+				remove(temp->value);
+			}
+		}
+	}
+
+	void preorder(node<T>* root) {
+		if (root == nullptr) return;
+		cout << root->value << " ";
+		preorder(root->left);
+		preorder(root->right);
+	}
+	void inorder(node<T>* root) {
+		if (root == nullptr) return;
+		preorder(root->left);
+		cout << root->value << " ";
+		preorder(root->right);
+
+	}
+	void postorder(node<T>* root) {
+		if (root == nullptr) return;
+		preorder(root->left);
+		preorder(root->right);
+		cout << root->value << " ";
+	}
+	void levelorder(node<T>* root) {
+		node<T>* temp;
+		queue_space::queue<node<T>*> *queue = new queue_space::queue<node<T>*>;
+		queue->enqueue(root);
+		while (queue->returnHead() != nullptr) {
+			temp = queue->dequeue();
+			cout << temp->value << " ";
+			if (temp->left != nullptr) {
+				queue->enqueue(temp->left);
+			}
+			if (temp->right != nullptr) {
+				queue->enqueue(temp->right);
 			}
 		}
 	}
 
 };
-/*
+
 int main() {
+
+	//red_black_tree<int> tree = red_black_tree<int>();
 	binary_tree<int> tree = binary_tree<int>();
 	node<int>* root;
 	int choice = -1, input = -1;
@@ -619,7 +385,7 @@ int main() {
 				cout << "empty";
 			}
 			else {
-				tree.remove(root, nullptr, input);
+				cout << tree.remove(input);
 			}
 			break;
 
@@ -654,26 +420,5 @@ int main() {
 		cout << endl;
 		if (check_out) break;
 	}
-	return 0;
-}
-*/
-
-int main() {
-	red_black_tree<int> tree = red_black_tree<int>();
-	tree.add(1);
-	tree.add(2);
-	tree.add(3);
-	tree.add(4);
-	tree.add(5);
-
-	tree.preorder(tree.getRoot());
-	cout << endl;
-	tree.inorder(tree.getRoot());
-	cout << endl;
-	tree.postorder(tree.getRoot());
-	cout << endl;
-	tree.levelorder(tree.getRoot());
-	cout << endl;
-
 	return 0;
 }
